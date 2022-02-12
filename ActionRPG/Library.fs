76bc71fbcb8b295fe139b82b124d9e1c12e3ec12
Nguyen Manh Quynh <quynh.fullstackdev@gmail.com>
2022-02-13 01:00:19 +0700

    Add lib for actionrpg project

---
+++ ActionRPG/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace ActionRPG
+
+open UnityEngine
+
+type ButtonManager() =
+   inherit MonoBehaviour()
+   
+   [<SerializeField>]
+   [<DefaultValue>] val mutable _informationText : GameObject
+
+    member this.Start() = 
+        Debug.Log("F# ")
+
+    member this.Update() = 
+        Debug.Log("hello man")
+
