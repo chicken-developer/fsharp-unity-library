b97f0f809533cfc651d2422e52c0b007034f93db
Nguyen Manh Quynh <quynh.fullstackdev@gmail.com>
2022-02-17 10:30:33 +0700

    Add lib for actionRPG game and Football game

---
+++ FootBall/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace Football
+
+open UnityEngine
+
+type TestFuture() =
+   inherit MonoBehaviour()
+   
+   [<SerializeField>]
+   [<DefaultValue>] val mutable _informationText : GameObject
+
+    member this.Start() = 
+        Debug.Log("Say hello from FSharp 222 ")
+
+    member this.Update() = 
+        Debug.Log "Enter new scene"
+
